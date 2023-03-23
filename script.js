bffbc83c99e75246fd3f3fb85bd64990e6715d26
connorb023 <126698422+connorb023@users.noreply.github.com>
2023-03-23 14:23:00 -0600

    added category function an dropdown menu

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    let categories; // Define the categories variable outside the ajax function
+    let percentageScore; // Define percentageScore as a global variable
+    let wrongAnswers = []; // Define an array to store the questions the user got wrong
+  
+    // Load the quiz categories on the page
+    $.ajax({
+      url: "https://opentdb.com/api_category.php",
+      method: "GET",
+      success: function(response) {
+        // Handle the response from the API
+        categories = response.trivia_categories;
+  
+        // Add each category to the dropdown menu
+        categories.forEach(function(category) {
+          let option = $("<option>").attr("value", category.id).text(category.name);
+          $("#category-select").append(option);
+        });
+      },
